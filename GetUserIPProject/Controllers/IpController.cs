940141b36b776149f6ae765a179313f6574a3879
nhan <nhoang2929@gmail.com>
2025-08-17 11:15:10 +0700

    add: get ip controller

---
+++ GetUserIPProject/Controllers/IpController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace GetUserIPProject.Controllers
+{
+    [Route("api/ip")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = "api")]
+    public class IpController : ControllerBase
+    {
+        private readonly ILogger<IpController> _logger;
+        public IpController(ILogger<IpController> logger)
+        {
+            this._logger = logger;
+        }
+
+        [HttpGet("health-check")]
+        [AllowAnonymous]
+        public IActionResult HealthCheck()
+        {
+            return Ok(new
+            {
+                message = "Good",
+                time = DateTime.Now.ToString()
+            });
+        }
+
+        [HttpGet("current")]
+        public IActionResult GetUserIp()
+        {
+            string ipClient = "";
+            string port = HttpContext.Connection.RemotePort.ToString();
+
+            try
+            {
+                ipClient = HttpContext.Connection.RemoteIpAddress.ToString();
+            } catch (NullReferenceException ex) 
+            {
+                this._logger.LogInformation($"Can not get information of ip, details: {ex.Message}");
+                return NotFound(new
+                {
+                    message = "Not found your ip address, please check again",
+                    ip = "",
+                    port = port
+                });
+            }
+            
+            Dictionary<string, object> response = new Dictionary<string, object>()
+            {
+                {"message", "SUCCESS" },
+                {"ip", ipClient },
+                {"port", port }
+            };
+
+            try
+            {
+                string json = JsonSerializer.Serialize(response);
+                // Log information of response
+                this._logger.LogInformation($"Get ip address complete {json}");
+            } catch (NotSupportedException notSupport)
+            {
+                this._logger.LogInformation($"Response type not support: {notSupport.Message}");
+                return BadRequest(new
+                {
+                    message = "Can not cast data into json type, please try again",
+                    ip = "",
+                    port = port
+                });
+            }
+            return Ok(response);
+        }
+    }
+}
---
+++ GetUserIPProject/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddAuthentication().AddJwtBearer("api", jwtOptions =>
+{
+    jwtOptions.TokenValidationParameters = new TokenValidationParameters
+    {
+     
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
